82af3c13894b20aed60ad5533578401c61d720fc
nadundev <nadunnissankauiux@gmail.com>
2025-07-21 00:31:22 +0530

    fixing build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import FloatingNav from "@/components/modules/floating-nav";
 import Hero from "@/components/sections/Hero";
-import FontTest from "@/components/ui/FontTest";
 
 export default function Home() {
   return (
